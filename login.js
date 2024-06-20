2b8e09ab7430c431c9e6e0b1e25b9270850d3928
Salmanquadri <secretofsk@gmail.com>
2024-06-20 17:03:07 +0530

    login page

---
+++ login.js
@@ -0,0 +1,7 @@
+let imglogin1 = document.querySelector(".imglogin1");
+function changescreen() {
+  imglogin1.style.right = "350px";
+}
+function changescreen1() {
+  imglogin1.style.right = "920px";
+}
